b35df201c8123f7b3d84449274c64e3b724918d6
Michael P Hanks <mphanks@msn.com>
2017-04-17 21:02:54 -0600

    moved nav to new headerDirective. hooked up view/directive cntrollers

---
+++ public/app/views/account/accountCtrl.js
@@ -0,0 +1,9 @@
+// INITILIZE CONTROLLER
+// ============================================================
+angular.module("rmaes").controller("accountCtrl", function($scope) {
+  // VARIABLES
+  // ============================================================
+  $scope.ctrl = "account";
+  // FUNCTIONS
+  // ============================================================
+});
