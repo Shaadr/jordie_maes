b30ff55883ba7a5d915209377b76c8c5638ca64c
Michael P Hanks <mphanks@msn.com>
2017-04-17 20:32:57 -0600

    added basic views/router. some navbar sass

--- public/app/app.js
+++ public/app/app.js
@@ -2,17 +2,38 @@
 // ============================================================
 angular.module("jmaes", ['ui.router']).config(function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('/');
-
   // INITILIZE STATES
   // ============================================================
   $stateProvider
     // HOME STATE
     .state('home', {
-      url: '/home',
+      url: '/',
       templateUrl: './app/views/home/home.html',
       controller: 'homeCtrl'
+    })
+    // SHOP STATE
+    .state('shop', {
+      url: '/shop',
+      templateUrl: './app/views/shop/shop.html',
+      controller: 'shopCtrl'
+    })
+    // about STATE
+    .state('about', {
+      url: '/about',
+      templateUrl: './app/views/about/about.html',
+      controller: 'aboutCtrl'
+    })
+    // account STATE
+    .state('account', {
+      url: '/account',
+      templateUrl: './app/views/account/account.html',
+      controller: 'accountCtrl'
     });
+
+
+
+
+
 
   // ASSIGN OTHERWISE
   // ============================================================
